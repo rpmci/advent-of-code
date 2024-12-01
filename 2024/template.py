6ac8c64154b8fa4917caa47a8b591589439769b0
Reid McInroy <reid.mcinroy@gmail.com>
2024-12-01 01:21:42 -0500

    Add scripts to create daily folder

---
+++ 2024/template.py
@@ -0,0 +1,19 @@
+def part1(lines):
+    return
+
+def part2(lines):
+    return
+
+def main():
+    with open('example-input.txt') as f:
+        lines = f.read().splitlines()
+        print("Part 1 Example Answer: {}".format(part1(lines)))
+        print("Part 2 Example Answer: {}".format(part2(lines)))
+
+    with open('input.txt') as f:
+        lines = f.read().splitlines()
+        print("Part 1 Example Answer: {}".format(part1(lines)))
+        print("Part 2 Example Answer: {}".format(part2(lines)))
+        
+if __name__ == "__main__":
+    main()
